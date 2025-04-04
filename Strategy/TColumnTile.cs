a944706755951fdca0d4ae951601fbe5a596fd67
Robert Świta <64035764+robertswita@users.noreply.github.com>
2025-04-04 15:24:40 +0200

    Add files via upload

---
+++ Strategy/TColumnTile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class TColumnTile : TSprite
+    {
+        public List<TCell> Cells = new List<TCell>();
+        public List<Image> Images;
+        //public int X_;
+        //public int Y_;
+
+        public override void Draw(Graphics gc)
+        {
+            for (var n = 0; n < Cells.Count; n++)
+            {
+                var cell = Cells[n];
+                gc.DrawImage(Images[cell.Piece.ImageIndex], X, Y + n * TCell.Height);
+                //if (CellProps != null && CellProps[Cells.Count + n] == 0)
+                //{
+                //    gc.DrawRectangle(Pens.Magenta, cell.X, cell.Y, TGame.TileWidth, TGame.TileHeight);
+                //}
+            }
+        }
+    }
+}
---
+++ Strategy/TMonster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class TMonster: TSprite
+    {
+        public int LootSlot1Id;
+        public int LootSlot1Type;
+        public int LootSlot2Id;
+        public int LootSlot2Type;
+        public int LootSlot3Id;
+        public int LootSlot3Type;
+    }
+}
