981b58c499317f420e2f13e54c2433d424d01d28
Robert Świta <64035764+robertswita@users.noreply.github.com>
2025-10-10 14:48:04 +0200

    Add files via upload

---
+++ Strategy/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Strategy
+{
+    public partial class StrategyForm : Form
+    {
+        TGame Game;
+        public float ScrollStep {  get { return 0.01f / Board.Zoom; } }
+        [DllImport("winmm.dll")] public static extern uint timeBeginPeriod(uint uMilliseconds);
+        [DllImport("winmm.dll")] public static extern uint timeEndPeriod(uint uMilliseconds);
+
+        public StrategyForm()
+        {
+            InitializeComponent();
+            //timeBeginPeriod(1);
+            Game = new TGame();
+            Board.Game = Game;
+            var map = new TMap();
+            map.Game = Game;
+            Game.MapView = new Bitmap(MapView.Width, MapView.Height);
+            Board.MouseWheel += TBoard1_MouseWheel;
+            Board.Scroll += BoardScroll;
+            Game.Restart();
+        }
+
+        private void BoardScroll(object sender, ScrollEventArgs e)
+        {
+            MapView.Invalidate();
+        }
+
+        private void MapView_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(Game.MapView, 0, 0);
+            var scaleX = MapView.Width / (Board.Game.Map.Width * TCell.Scale.X);
+            var scaleY = MapView.Height / (Board.Game.Map.Height * TCell.Scale.Y);
+            e.Graphics.ScaleTransform(scaleX, scaleY / 2);
+            e.Graphics.DrawRectangle(Pens.Red, Rectangle.Round(Board.Viewport));
+            MapNameLbl.Text = Game.Map.MapName;
+        }
+
+        private void PlayTimer_Tick(object sender, EventArgs e)
+        {
+            if (Cursor.Position.X < Margin.Left)
+                Board.ScrollPos -= new SizeF(ScrollStep, 0);
+            else if (Cursor.Position.X > Width - Margin.Right)
+                Board.ScrollPos += new SizeF(ScrollStep, 0);
+            if (Cursor.Position.Y < Margin.Top)
+                Board.ScrollPos -= new SizeF(0, ScrollStep);
+            else if (Cursor.Position.Y > Height - Margin.Bottom)
+                Board.ScrollPos += new SizeF(0, ScrollStep);
+            foreach (var sprite in Game.Sprites)
+            {
+                var actFrame = sprite.ActFrame;
+                sprite.NextFrame();
+                if (actFrame != sprite.ActFrame)
+                    Board.Invalidate();
+            }
+            Game.ActiveScript?.Run();
+        }
+
+        private void MapView_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                var p = new PointF(e.X, e.Y);
+                p.X /= MapView.Width;
+                p.Y /= MapView.Height;
+                Board.ScrollPos = p;
+            }
+        }
+
+        private void TBoard1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            base.OnMouseWheel(e);
+            if (e.Delta < 0)
+                Board.Zoom *= 0.9f;
+            else
+                Board.Zoom *= 1.1f;
+            var timerInterval = (int)(100 / Board.Zoom);
+            var minTime = 1000 / TGame.FPS;
+            if (timerInterval < minTime) timerInterval = minTime;
+            PlayTimer.Interval = timerInterval;
+            Board.Invalidate();
+            MapView.Invalidate();
+        }
+
+        private void HerosList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Board.Game.ActivePlayer.SelectedHero = Board.Game.ActivePlayer.Heroes[HerosList.SelectedIndex];
+            var hero = Board.Game.ActivePlayer.SelectedHero;
+            var p = new PointF(hero.Cell.X, hero.Cell.Y);
+            p.X /= Board.Game.Map.Width;
+            p.Y /= Board.Game.Map.Height;
+            Board.ScrollPos = p;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Board.Game.NextTurn();
+            HerosList.DataSource = Board.Game.ActivePlayer.Heroes;
+            HerosList.DisplayMember = "Name";
+            HerosList.BackColor = Board.Game.ActivePlayer.ID;
+        }
+
+        private void ResourceChanged(object sender, EventArgs e)
+        {
+            var activePlayer = Board.Game.ActivePlayer;
+            var names = Enum.GetNames(typeof(TResource.ResType));
+            ResourceView.Items.Clear();
+            for (int i = 0; i < activePlayer.Resources.Length; i++)
+            {
+                var item = new ListViewItem(names[i], i);
+                item.SubItems.Add(activePlayer.Resources[i].ToString());
+                ResourceView.Items.Add(item);
+            }
+        }
+
+        private void StrategyForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.T: ImportMap(); break;
+            }
+        }
+
+        void ImportMap()
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Game = new TGame();
+                Game.MapView = new Bitmap(MapView.Width, MapView.Height);
+                Board.Game = Game;
+                Board.Game.ImportMap(openFileDialog1.FileName);
+                if (Path.GetExtension(openFileDialog1.FileName).ToLower() == ".bmp")
+                {
+                    Game.OnResourceChanged = ResourceChanged;
+                    HerosList.DataSource = Game.ActivePlayer.Heroes;
+                    HerosList.DisplayMember = "Name";
+                    HerosList.BackColor = Game.ActivePlayer.ID;
+                }
+                Board.Invalidate();
+                MapView.Invalidate();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ImportMap();
+        }
+
+        private void panel1_BackColorChanged(object sender, EventArgs e)
+        {
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            SetStyle(ControlStyles.Opaque, true);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var map = Game.Map as TDispelMap;
+                if (map != null)
+                    map.WriteMap(saveFileDialog1.FileName);
+            }
+
+        }
+
+        private void StrategyForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //timeEndPeriod(1);
+        }
+    }
+}
---
+++ Strategy/TScript.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class TScript
+    {
+        public TGame Game;
+        //TDispelMap Map;
+        public List<List<string>> Commands;
+        public Dictionary<string, int> Variables;
+        int ActCommandNo;
+        public bool IsWaiting;
+        int Delay;
+        bool Condition;
+        public void Run()
+        {
+            if (Delay > 0)
+            {
+                Delay--;
+                if (Delay == 0) IsWaiting = false;
+            }
+            if (IsWaiting) return;
+            if (ActCommandNo >= Commands.Count)
+            {
+                Game.ActiveScript = null;
+                return;
+            }
+            var command = Commands[ActCommandNo];
+            ActCommandNo++;
+            int arg1 = 0;
+            if (command.Count > 1 && command[1] != "" && !int.TryParse(command[1], out arg1))
+                arg1 = Variables[command[1]];
+            switch (command[0])
+            {
+                case "teleport":
+                    var x = int.Parse(command[1]);
+                    var y = int.Parse(command[2]);
+                    var mapIdx = int.Parse(command[3]);
+                    SetMap(mapIdx, x, y);
+                    break;
+                case "setmap":
+                    var scr = new TIniReader($"{TMap.GamePath}/Ref/Map.ini");
+                    var mapLine = scr[""][int.Parse(command[1])];
+                    x = int.Parse(mapLine[2]);
+                    y = int.Parse(mapLine[3]);
+                    mapIdx = int.Parse(mapLine[4]);
+                    SetMap(mapIdx, x, y);
+                    break;
+                case "chractivate":
+                    Game.MainChar.Enabled = int.Parse(command[1]) == 1;
+                    break;
+                case "chrmoveto":
+                    NpcMoveTo(Game.MainChar, int.Parse(command[1]), int.Parse(command[2]));
+                    break;
+                case "chrchangedir":
+                    Game.MainChar.ViewAngle = int.Parse(command[1]);
+                    break;
+                case "npcmoveto":
+                    var npc = Game.Npcs[arg1];
+                    NpcMoveTo(npc, int.Parse(command[2]), int.Parse(command[3]));
+                    break;
+                case "npcchangedir":
+                    npc = Game.Npcs[arg1];
+                    npc.ViewAngle = int.Parse(command[2]);
+                    break;
+                case "delay":
+                    IsWaiting = true;
+                    Delay = int.Parse(command[1]);
+                    break;
+                case "createnpc":
+                    Variables[command[1]] = Game.Npcs.Count;
+                    scr = new TIniReader($"{TMap.GamePath}/NpcInGame/Eventnpc.ref");
+                    var npcLine = scr[""][int.Parse(command[2])];
+                    //npc = Game.Npcs[int.Parse(npcLine[1])];
+                    var npcNew = new TNpc();
+                    npcNew.Game = Game;
+                    var pos = Game.Map.TransformGrid(int.Parse(npcLine[8]), int.Parse(npcLine[9]));
+                    npcNew.Cell = Game.Cells[(int)pos.Y, (int)pos.X];
+                    //npcNew.Animation = npc.Animation;
+                    npcNew.Animation = (Game.Map as TDispelMap).NpcSpriteModels[int.Parse(npcLine[1])];
+                    Game.Npcs.Add(npcNew);
+                    npcNew.Index = Game.Sprites.Count;
+                    Game.Sprites.Add(npcNew);
+                    break;
+                case "deletenpc":
+                    npc = Game.Npcs[arg1];
+                    Game.Sprites.RemoveAt(npc.Index);
+                    Game.Npcs.RemoveAt(arg1);
+                    break;
+                case "refreshnpc":
+                    break;
+                case "dialogtonpc":
+                    npc = Game.Npcs[arg1];
+                    Game.ActiveNpc = npc;
+                    Game.Board.DialogId = int.Parse(command[2]);
+                    Game.Board.ProcessDialog();
+                    IsWaiting = true;
+                    break;
+                case "dialog":
+                    //npc = Game.Npcs[arg1];
+                    Game.ActiveNpc = Game.MainChar;
+                    Game.Board.DialogId = arg1;
+                    Game.Board.ProcessDialog();
+                    IsWaiting = true;
+                    break;
+                case "if":
+                    Condition = arg1 == int.Parse(command[3]);
+                    if (!Condition) SkipBlock();
+                    break;
+                case "else":
+                    if (Condition) SkipBlock();
+                    break;
+                case "getitem":
+                    Game.ActivePlayer.Items.Add(Game.Items[arg1][int.Parse(command[2])]);
+                    break;
+                case "chkitem":
+                    Variables[command[1]] = Game.ActivePlayer.Items.Contains(Game.Items[int.Parse(command[2])][int.Parse(command[3])]) ? 1: 0;
+                    break;
+                case "return":
+                    Game.ActiveScript = null;
+                    break;
+            }
+        }
+
+        void SkipBlock()
+        {
+            var command = new List<string>() { "{" };
+            while (command[0] != "}")
+            {
+                command = Commands[ActCommandNo];
+                ActCommandNo++;
+            }
+        }
+
+        void SetMap(int mapIdx, float x, float y)
+        {
+            var mapName = Game.MapNames[mapIdx];
+            if (mapName != Game.Map.MapName)
+                Game.Map.ReadMap($"{TMap.GamePath}/Map/{mapName}.map");
+            var pos = (Game.Map as TDispelMap).TransformGrid(x, y);
+            pos.X /= Game.Map.Width;
+            pos.Y /= Game.Map.Height;
+            Game.Board.ScrollPos = new PointF(pos.X, pos.Y);
+        }
+
+        void NpcMoveTo(TNpc npc, float x, float y)
+        {
+            var destP = new Vector2(x, y);
+            if (destP.X != 0 || destP.Y != 0)
+                destP = Game.Map.TransformGrid(destP.X, destP.Y + 1);
+            var startP = new Vector2(npc.Cell.X, npc.Cell.Y);
+            if (startP != destP)
+            {
+                Game.ActiveNpc = npc;
+                npc.CalcPath(new List<Vector2>() { startP, destP });
+                IsWaiting = true;
+            }
+        }
+    }
+}
